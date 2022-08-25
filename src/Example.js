9f8b692c641e27a70c0d1e73607c63cb18164251
Kelly Meleshko <101250830+kellyjfitz@users.noreply.github.com>
2022-08-26 09:56:45 +1000

    Added attribution

--- src/Example.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-export default function Example(props) {
-  return <em></em>;
-}
--- src/Footer.js
+++ src/Footer.js
@@ -6,31 +6,49 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faGithubSquare } from "@fortawesome/free-brands-svg-icons";
 
+
+import { faImagePortrait } from "@fortawesome/free-solid-svg-icons";
+
 export default function Footer() {
   return (
     <footer>
-      <ul>
-        <li>
+      
           <a
             href="https://github.com/kellyjfitz/dictionary-app"
             target="_blank"
             title="See this site on GitHub"
             rel="noopener noreferrer"
+            className="icon"
           >
             <FontAwesomeIcon icon={faGithubSquare} />
           </a>
-        </li>
-        <li>
+        
+          <a
+            href="https://playful-strudel-6e76b8.netlify.app/portfolio.html"
+            target="_blank"
+            title="Kelly Meleshko's portfolio"
+            rel="noopener noreferrer"
+            className="icon"
+           
+          >
+            <FontAwesomeIcon icon={faImagePortrait} className="portfolio" />
+          </a>
+        
           <a
             href="https://au.linkedin.com/in/kelly-meleshko"
             target="_blank"
             title="Kelly Meleshko's LinkedIn"
             rel="noopener noreferrer"
+            className="icon"
           >
             <FontAwesomeIcon icon={faLinkedin} />
           </a>
-        </li>
-      </ul>
+        <br />
+      <cite>Photos provided by <a href="https://pexels.com" target="_blank"
+            title="Visit Pexels"
+            rel="noopener noreferrer">Pexels</a><br />Definitions provided by <a href="https://dictionaryapi.dev/" target="_blank"
+            title="Visit Free Dictionary API"
+            rel="noopener noreferrer">Free Dictionary API</a></cite>
     </footer>
   );
 }
